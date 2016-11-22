7c62b2cc8c71c03edda359781a97a894104ace14
Raphael Zublin <raphaelzublin@yahoo.com.br>
2016-11-22 19:31:26 -0300

    Revert "Node Express Server"
    This reverts commit 52db823d01e0a3d69ea6182681e897eb79472733.

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-app.use(express.static('public'));
-
-app.listen(3000, function(){
-    console.log("Express server running on port 3000");
-});
